9fc6ea0369329c79d3cc8ccb62200cb87f2a65c9
INBAR <inbar.haddad@gmail.com>
2020-12-10 11:11:44 +0200

    initial

--- app/src/main/java/com/example/inbar_application1/ui/MainGameScreen.java
+++ app/src/main/java/com/example/inbar_application1/ui/MainGameScreen.java
@@ -5,8 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +23,7 @@ import java.util.ArrayList;
 public class MainGameScreen extends AppCompatActivity implements CluesOnClick {
     private ImageView cluePic;
     private ClueAdapter clueAdapter;
+    private Button sendToNextScreen;
     private ArrayList<Clue> clues;
 
 
@@ -34,6 +40,7 @@ public class MainGameScreen extends AppCompatActivity implements CluesOnClick {
 
         clueAdapter = new ClueAdapter(clues, this);
         recyclerView.setAdapter(clueAdapter);
+
     }
     public boolean checkIfExists(String description){
         for (int i = 0; i < clues.size() ; i++) {
@@ -57,8 +64,7 @@ public class MainGameScreen extends AppCompatActivity implements CluesOnClick {
     public  void onClickItem(View view){
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.clue_dialog);
-//        Dialog moveScreen = new Dialog(this);
-//        dialog.setContentView(R.layout.moving_screen_dialog);
+
         Clue clue = new Clue(1, view.getTag().toString(),0);
         if(view.getTag().toString().equals("key") && !checkIfExists("key")){
             clue.setIcon(R.drawable.clue1);
@@ -94,10 +100,20 @@ public class MainGameScreen extends AppCompatActivity implements CluesOnClick {
             dialog.show();
             clueAdapter.notifyDataSetChanged();
         }
-
-
-
+        if (view.getTag().toString().equals("done")){
+            if (checkIfAllCollected()){
+                dialog.setContentView(R.layout.moving_screen_dialog);
+                EditText edit=(EditText)dialog.findViewById(R.id.type);
+                String text=edit.getText().toString();
+                if(text.equals("attic")){
+                    startActivity(new Intent(MainGameScreen.this, AtticScreen.class));
+                }
+            }
+            dialog.show();
+        }
     }
+
+
 
 
     @Override
